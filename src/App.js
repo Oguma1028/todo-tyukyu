647def59c9f80e4ce62e56cd4a083f4601df097a
Oguma1028 <neetoguma@gmail.com>
2022-08-27 18:14:59 +0900

    TodoItemをApp.js内に追加

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import TodoList from "./components/TodoList";
 import Sort from "./components/Sort";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   return (
     <>
       <TodoList />
       <Sort />
+      <TodoItem />
     </>
   );
 }
